d77a316f33ea73079588eda7f5cd8589b493256a
Diony-source <erendalkiran2001@gmail.com>
2024-12-27 22:31:29 +0300

    feat(utils): Add API request handler for fetching news

---
+++ utils/request.go
@@ -0,0 +1,26 @@
+package utils
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func MakeAPIRequest(apiURL string) ([]byte, error) {
+	resp, err := http.Get(apiURL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to make API request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("API request failed with status: %d", resp.StatusCode)	
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read API response: %v", err)
+	}
+
+	return body, nil
+}
